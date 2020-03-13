fee5477a01fb0d7400e5186afe32e6e4438a544a
AntoineGautier <agautier@lbl.gov>
2020-03-12 18:11:30 -0700

    Auto commit all [ci skip]

--- specification/source/architecture.rst
+++ specification/source/architecture.rst
@@ -3,10 +3,20 @@
 Software Architecture
 ---------------------
 
-:numref:`linkage_architecture_legend` to :numref:`linkage_architecture_os` provide simplified architecture diagrams for the three integration targets.
-Those are not hard and fast. They rather aim at describing the main modules to develop and how they interface with LBL or third party developments.
+:numref:`linkage_architecture_legend` to :numref:`linkage_architecture_os` provide architecture diagrams for the three integration targets.
+Those are not hard and fast. They rather aim at illustrating the data workflow and describing the main modules to develop, and how they interface with LBL or third party developments.
 
-`To be updated: add definitions cf. Michael's comments.`
+The following definitions and conventions are used.
+
+  Config data: pieces of data provided as input to the configuration widget, see :numref:`sec_data_model` for definitions and :numref:`sec_annex_json` for an actual example.
+
+  Model data: pieces of data used by the graphical editor to allow manipulating a Modelica model. Those correspond to the Modelica code as interpreted by the editor according to its specific data model.
+
+  Return *<arg>*: describes the translating task from one language (e.g. Modelica or Brick) or one data model (e.g. Modelica raw code or JSON) to another, specified as *<arg>*.
+
+  Input: describes the user action of setting a parameter value through mouse or keyboard input.
+
+  Call >> return *<arg>*: describes a bidirectional task where a component A calls another component B, and B returns *<arg>* to A.
 
 
 .. figure:: img/linkage_architecture_legend.*
@@ -15,19 +25,25 @@ Those are not hard and fast. They rather aim at describing the main modules to d
    Software architecture legend
 
 
+.. figure:: img/linkage_architecture_plugin.*
+   :name: linkage_architecture_plugin
+
+   Software architecture for the configuration widget integrated as a plugin to an existing graphical editor for Modelica
+
+
 .. figure:: img/linkage_architecture.*
    :name: linkage_architecture
 
-   Software architecture - Desktop app
+   Software architecture for the full-featured graphical editor embedding the configuration widget - Desktop app
 
 
 .. figure:: img/linkage_architecture_spa.*
    :name: linkage_architecture_spa
 
-   Software architecture - Standalone web app
+   Software architecture for the full-featured graphical editor embedding the configuration widget - Standalone web app
 
 
 .. figure:: img/linkage_architecture_os.*
    :name: linkage_architecture_os
 
-   Software architecture - Integration in third party application e.g. `OpenStudio® <https://www.openstudio.net>`_ (OS)
+   Software architecture for the full-featured graphical editor embedding the configuration widget - Integration in third party application e.g. `OpenStudio® <https://www.openstudio.net>`_ (OS)
--- specification/source/conf.py
+++ specification/source/conf.py
@@ -23,7 +23,7 @@ extensions = [
 
 # Rst_prolog is a string of reStructuredText that will be included at the beginning of every source file that is read.
 
-rst_prolog = """
+rst_prolog = '''
 .. role:: underline
    :class: underline
 
@@ -32,10 +32,10 @@ rst_prolog = """
 .. |doc_title| replace:: {doc_title}
 
 .. |doc_version| replace:: {doc_version}
-""".format(project=project, doc_title=doc_title, doc_version=doc_version)
+'''.format(project=project, doc_title=doc_title, doc_version=doc_version)
 
 # mathjax_path
-mathjax_path = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 
 todo_include_todos = True
 
@@ -80,27 +80,26 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 html_theme_options = {
     'navbar_title': project,
     'nosidebar': True,
+    'body_min_width': '100%',
     # Render the next and previous page links in navbar. (Default: true)
     'navbar_sidebarrel': True,
     # Render the current pages TOC in the navbar. (Default: true)
     'navbar_pagenav': True,
-    # Tab name for the current pages TOC. (Default: "Page")
-    'navbar_site_name': "Site",
-    'navbar_pagenav_name': "Page",
-    # Global TOC depth for "site" navbar tab. (Default: 1)
+    # Tab name for the current pages TOC. (Default: 'Page')
+    'navbar_site_name': 'Site',
+    'navbar_pagenav_name': 'Page',
+    # Global TOC depth for 'site' navbar tab. (Default: 1)
     # Switching to -1 shows all levels.
     'globaltoc_depth': 3,
     # Include hidden TOCs in Site navbar?
     'globaltoc_includehidden': True,
-    # HTML navbar class (Default: "navbar") to attach to <div> element.
-    # For black navbar, do "navbar navbar-inverse"
-    'navbar_class': "navbar",
+    # HTML navbar class (Default: 'navbar') to attach to <div> element.
+    # For black navbar, do 'navbar navbar-inverse'
+    'navbar_class': 'navbar',
     # Fix navigation bar to top of page?
     'navbar_fixed_top': True,
     # Location of link to source.
-    'source_link_position': "footer",
-    # Bootswatch (http://bootswatch.com/) theme.
-    # 'bootswatch_theme': "united",
+    'source_link_position': 'footer',
 }
 
 html_last_updated_fmt = '%b, %d, %Y'
@@ -111,11 +110,11 @@ html_title = doc_title
 # The name of an image file (within the static path) to use as favicon of the
 # docs. This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "_static/lbl-icon.ico"
+html_favicon = '_static/lbl-icon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
 html_static_path = ['_static']
 
 # If true, links to the reST sources are added to the pages.
